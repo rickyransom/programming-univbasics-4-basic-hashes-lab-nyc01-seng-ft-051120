b1e4fc53b0fcbde19d5386b421199bb16021ae10
Jean-Richard Latortue <rickyransomco@gmail.com>
2020-05-03 16:55:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,6 @@ end
 
 id_hash_generator = {}
 def id_hash_generator(number)
-  id_hash_generator = {:id => 451}
+  id_hash_generator = {:id => number}
   # return a hash with a key :id assigned to the provided number
 end
